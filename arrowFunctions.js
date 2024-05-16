75a479fc89cea053bc672cddcd2f3bfcffb28f8c
Sanket Suryavanshi <sanket20020312@gmail.com>
2024-05-16 18:39:46 +0530

    Arrow Functions

---
+++ arrowFunctions.js
@@ -0,0 +1,19 @@
+/*
+
+The normal functions which we write are as below:
+
+function sum(a,b){
+    return a+b;
+}
+
+console.log(sum(1,2));
+
+*/
+
+// arrow functions
+
+const sum = (a,b) =>{
+    return a+b;
+}
+
+console.log(sum(1,2))
